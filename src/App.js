ac1ea29c0514f1284619d7fa87c0c016af55b9cd
luizzmy <luisg.valenzuelam@gmail.com>
2021-11-02 16:00:14 -0600

    useQuery hook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   const [page, setPage] = useState('planets')
 
   return (
+    <QueryClientProvider client={queryClient}>
     <div className="App">
       <h1>Star Wars Info</h1>
       <Navbar setPage={setPage}/>
@@ -17,6 +21,7 @@ function App() {
       </div>
 
     </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/People.js
+++ src/components/People.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import Person from './Person'
+
+const fetchPeople = async () => {
+    const res = await fetch("http://swapi.dev/api/people/")
+    return res.json()
+}
 
 const People = () => {
+
+    const { data, status } = useQuery('people', fetchPeople)
+
+    console.log(data)
+
     return (
         <div>
             <h2>People</h2>
+            {status === "error" ?
+                <div>There was an error</div>
+                :
+                status === "loading" ?
+                    <div>Fetch is loading</div>
+                    :
+                    <div>{data.results.map(person=><Person key={person.name} person={person}/>)}</div>}
         </div>
     )
 }
 
 export default People
+
